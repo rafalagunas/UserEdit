8b91098b8a8f3b9f82083c54fa069b283b08ad5f
Rafael Lagunas <rafael@imviatori.com>
2018-01-04 13:12:27 -0500

    dispatch

--- src/components/UserEdit/UserEdit.js
+++ src/components/UserEdit/UserEdit.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 
 import {connect} from 'react-redux';
+import { update } from '../../actions/UserEdit';
+
 
 class UserEdit extends Component {
     constructor (props) {
@@ -22,24 +24,32 @@ class UserEdit extends Component {
         };
     }
 
+    userUpdate (e) {
+        this.props.userUpdate(this.state.username, this.state.name, this.state.lastname, this.state.email);
+        e.preventDefault();
+    }
     render() {
         return(
             <View > 
                 <TextInput 
                     placeholder="Username"
                     onChangeText={(text) => this.setState({ username: text })}
+                    value={this.state.username}
                     />
                 <TextInput 
                     placeholder="Nombre"
                     onChangeText={(text) => this.setState({ name: text })}
+                    value={this.state.name}
                     />
                 <TextInput 
                     placeholder="Apellido"
                     onChangeText={(text) => this.setState({ lastname: text })}
+                    value={this.state.lastname}
                     />
                 <TextInput 
                     placeholder="Correo"
                     onChangeText={(text) => this.setState({ email: text })}
+                    value={this.state.email}
                     />
 
                 <Button
@@ -50,4 +60,19 @@ class UserEdit extends Component {
     }
 }
 
-export default UserEdit;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        userUpdate: (username, name, lastname, email) => { dispatch(update(username, name, lastname, email)); },
+    }
+}
+ 
+const mapStateToProps = (state, ownProps) => {
+    return {
+        username: state.auth.username,
+        name: state.auth.name,
+        lastname: state.auth.lastname,
+        email: state.auth.email
+    };
+}
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(UserEdit);
